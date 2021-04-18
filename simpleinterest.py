5a94b5ba84885eb494836ba257546e7b7b84283b
kamalesh.B <82257435+kamalesh20-hacker@users.noreply.github.com>
2021-04-18 19:49:52 +0530

    Add files via upload

---
+++ simpleinterest.py
@@ -0,0 +1,12 @@
+P = int(input("enter the principle value:"))
+R = int(input("enter the rate value:"))
+T = int(input("enter the year:"))
+# simple interest
+SI = (P * R * T) / 100
+print("simple interest is", SI)
+
+#output
+#enter the principle value:1000
+#enter the rate value:1
+#enter the year:2
+#simple interest is 20.0
